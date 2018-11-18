824601f9af2eb4bd3ee88202fe0bbff8ff30b52d
Maciek Wróblewski <maciekwroblewski@MacBook-Pro-Maciek.local>
2018-11-18 22:08:03 +0100

    Add new view for habbit btn.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Master of habbit'),
+        ),
+        body: Habit(),
+      ),
     );
   }
 }
